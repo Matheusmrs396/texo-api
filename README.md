5aa6474222f9b423975bd2947aa5675ee55146cc
Matheus Silva <matheusmrs396@gmail.com>
2022-10-11 13:51:44 -0300

    Criando testes de integração para os metodos http e alterando README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 Para rodar o projeto:
-- Foram utilizados JDK 17, Maven 3.8.6 e Spring Boot 2.7.4 para o desenvolvimento
+- Foram utilizados JDK 17, Maven 3.8.6 e Spring Boot 2.7.4 para o desenvolvimento;
+- Após baixar o projeto e abri-lo em uma IDE, execute um clean install no Maven para que baixe as dependencias necessárias para sumirem os erros;
 - Basta acessar a classe TexoMoviesApplication localizada em src/main/java/com/api/texomovies/TexoMoviesApplication.java e clicar em "run";
-- O endpoint está localizado na url http://localhost:8080/movies/awards-interval
+- O endpoint com a proposta da api está localizado na url http://localhost:8080/movies/awards-interval usando o método GET do padrão HTTP;
+- Endpoint de listagem de todos os filmes localizado na url http://localhost:8080/movies usando o método GET do padrão HTTP;
+- Endpoint para cadastrar filme localizado na url http://localhost:8080/movies usando o método POST do padrão HTTP;
+- Endpoint para excluir filme localizado na url http://localhost:8080/movies/{idDoFilme} usando o método DELETE do padrão HTTP;
+- Endpoint para atualizar(editar) filme localizado na url http://localhost:8080/movies/{idDoFilme} usando o método PUT do padrão HTTP;
+- Endpoint para buscar um filme localizado na url http://localhost:8080/movies/{idDoFilme} usando o método GET do padrão HTTP;
 - Caso queira verificar o banco basta acessar a url http://localhost:8080/h2 user:sa password:
 
 
--- src/test/java/com/api/texomovies/MovieControllerTest.java
+++ src/test/java/com/api/texomovies/MovieControllerTest.java
@@ -1,11 +1,15 @@
 package com.api.texomovies;
 
 import com.api.texomovies.controller.MovieController;
+import com.api.texomovies.entity.Movie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,10 +27,53 @@ public class MovieControllerTest extends TexoMoviesApplicationTests{
     }
 
     @Test
-    public void should_return_status_ok() throws Exception {
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/movies/awards-interval"))
+    public void should_get_awards_interval_return_status_ok() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/movies/awards-interval"))
                  .andExpect(MockMvcResultMatchers.status().isOk())
                  .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("producer")));
+    }
+
+    @Test
+    public void should_create_movie_return_status_created() throws Exception {
+        Movie movie = new Movie(200L, 2023, "any_movie", "any_studio","any_producer", false);
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json = mapper.writeValueAsString(movie);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/movies")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                    .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void should_update_movie_return_status_no_content() throws Exception {
+        Movie movie = new Movie(null, 2027, "any_movie", "any_studio","any_producer", false);
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json = mapper.writeValueAsString(movie);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/movies/10")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+    
+    @Test
+    public  void should_get_movie_by_id_return_status_ok() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/movies/200"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void should_delete_movie_return_status_no_content() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.delete("/movies/200"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    public void should_get_all_movies_return_status_ok() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/movies"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
